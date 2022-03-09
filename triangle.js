7beddcc1ecba95cabf2909a2abb8bfedd5037a8b
Binbo55 <hieuduke123@gmail.com>
2022-03-09 17:51:43 +0700

    report 09/03

---
+++ triangle.js
@@ -0,0 +1,36 @@
+/* exer1
+let a = "#"
+for (i = 1; i < 6; i++) {
+    console.log(a = a + "#")
+} 
+*/
+
+/* exer2
+ for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+        console.log(i + "Fizz")
+    } else if (i % 5 === 0) {
+        console.log(i + "Buzz")
+    } else(console.log(i))
+} 
+*/
+
+/* exer3
+let size = 6;
+
+let chess = ""
+
+for (let i = 1; i <= size; i++) {
+    for (let j = 1; j <= size; j++) {
+        if (i % 2 === 0) {
+            chess += "# "
+        } else {
+            chess += " #"
+        }
+    }
+    chess += "\n"
+}
+
+
+console.log(chess) 
+*/
